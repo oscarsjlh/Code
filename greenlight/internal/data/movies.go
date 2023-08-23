87695f1bb90e9b21f993298fff2d8fece2ea82ce
oscar <oscarsjlh@gmail.com>
2023-08-23 19:16:33 +0100

    add files

--- greenlight/internal/data/movies.go
+++ greenlight/internal/data/movies.go
@@ -25,7 +25,7 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	query := `INSERT INTO movies (title, year, runtime, generes)
+	query := `INSERT INTO movies (title, year, runtime, genres)
 	VALUES ($1, $2, $3, $4)
 	RETURNING id, created_at, version`
 	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
@@ -33,8 +33,8 @@ func (m MovieModel) Insert(movie *Movie) error {
 }
 
 func (m MovieModel) Update(movie *Movie) error {
-	query := `UPDATE movies SET title = $1, year = $2, runtime = $3, generes = $4, version = version + 1
-        WHERE id = $5
+	query := `UPDATE movies SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+        WHERE id = $5 AND version = $6
         RETURNING version`
 	args := []any{
 		movie.Title,
@@ -42,8 +42,18 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
@@ -88,7 +98,7 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 	if id < 1 {
 		return nil, ErrRecordNotFound
 	}
-	query := `SELECT id, created_at, title, year, runtime, generes, version
+	query := `SELECT id, created_at, title, year, runtime, genres, version
 	FROM movies
 	WHERE id = $1`
 
